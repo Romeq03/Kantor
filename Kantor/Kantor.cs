257d6445c8e819f2568b380896544ce5fed6bb16
Bartłomiej Romek <148621924+Romeq03@users.noreply.github.com>
2024-01-28 15:06:59 +0100

    Update Kantor.cs

--- Kantor/Kantor.cs
+++ Kantor/Kantor.cs
@@ -4,23 +4,19 @@ class CurrencyConverter
 {
     static void Main()
     {
-        // Kursy wymiany walut (zakodowane na stałe)
         double plnToUsdExchangeRate = 0.25;
         double usdToEurExchangeRate = 0.85;
         double usdToChfExchangeRate = 0.92;
         double usdToGbpExchangeRate = 0.75;
 
-        // Pobieranie danych od użytkownika
         Console.Write("Podaj kwotę w PLN: ");
         double amountInPln = Convert.ToDouble(Console.ReadLine());
 
         Console.Write("Podaj walutę docelową (EUR, CHF, GBP): ");
         string targetCurrency = Console.ReadLine().ToUpper();
 
-        // Przeliczanie kwoty na walutę pośrednią (USD)
         double amountInUsd = amountInPln * plnToUsdExchangeRate;
 
-        // Przeliczanie na walutę docelową
         double result = 0.0;
         switch (targetCurrency)
         {
@@ -38,10 +34,7 @@ class CurrencyConverter
                 return;
         }
 
-        // Wyświetlanie wyniku
         Console.WriteLine($"Wynik przeliczenia: {result} {targetCurrency}");
-
-        // Oczekiwanie na naciśnięcie klawisza przed zakończeniem programu
         Console.ReadLine();
     }
 }
